8aa002e410972abb771ecc47ec9e1f59ccab1361
progand <lach@ukr.net>
2015-12-23 19:14:03 +0200

    [*]  text neuron all blocks from js fixture data

--- app/text/text.js
+++ app/text/text.js
@@ -5,16 +5,15 @@ angular.module('myApp.text', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/text', {
             templateUrl: 'text/text.html',
-            controller: 'TextCtrl'
+            controller: 'TextCtrl',
+            resolve: {
+                neuron: function () {
+                    return window['text-neuron-data'];
+                }
+            }
         });
     }])
 
-    .controller('TextCtrl', ['$scope', function ($scope) {
-        $scope.neuron = {
-            title: "What is LivaRava",
-            subscribers: 15,
-            axons: 35,
-            views: 216
-
-        }
+    .controller('TextCtrl', ['$scope', 'neuron', function ($scope, neuron) {
+        $scope.neuron = neuron;
     }]);
