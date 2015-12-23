594ecb970f40e1b21bbf8b4829a6e4c76d1f0eaa
progand <lach@ukr.net>
2015-12-23 19:14:03 +0200

    [*]  text neuron all blocks from js fixture data

---
+++ app/fixtures/text-neuron.js
@@ -0,0 +1,174 @@
+(function () {
+    var neuron = {
+        title: "What is LivaRava",
+        subscribers: 15,
+        axons: 35,
+        views: 216,
+        neurons: [{
+            "id": 22508,
+            "weight": 11,
+            "header": "How startup funding works - Infographics",
+            "title": "https://wantrepreneurship.files.wordpress.com/2015/04/how-funding-works-infographic.png",
+            "type": "image",
+            "data": 22508,
+            "icon": "/static/livarava/img/neurons/image.png",
+            "image": "https://wantrepreneurship.files.wordpress.com/2015/04/how-funding-works-infographic.png",
+            "type_title": "image"
+        }, {
+            "id": 27462,
+            "weight": 10,
+            "header": "Fundraising",
+            "title": "Fundraising",
+            "type": "text",
+            "data": 27462,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 27822,
+            "weight": 9,
+            "header": "Startup fundraising",
+            "title": "http://techcrunch.com/2015/06/08/lessons-from-a-study-of-perfect-pitch-decks-vcs-spend-an-average-of-3-minutes-44-seconds-on-them/#.ypshg9:qYdc",
+            "type": "link",
+            "data": 27822,
+            "icon": "/static/livarava/img/neurons/link.png",
+            "image": "/static/livarava/img/neurons/link.png",
+            "type_title": "link"
+        }, {
+            "id": 32183,
+            "weight": 5,
+            "header": "Are You Ready to Seek Funding? This 10-Point Checklist Will Decide.",
+            "title": "http://www.entrepreneur.com/article/248777",
+            "type": "link",
+            "data": 32183,
+            "icon": "/static/livarava/img/neurons/link.png",
+            "image": "/static/livarava/img/neurons/link.png",
+            "type_title": "link"
+        }, {
+            "id": 22518,
+            "weight": 5,
+            "header": "Funding",
+            "title": "Funding",
+            "type": "text",
+            "data": 22518,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 0,
+            "weight": 299,
+            "header": "LivaRava",
+            "title": "LivaRava",
+            "type": "project",
+            "data": 0,
+            "icon": "/static/livarava/img/neurons/project.png",
+            "image": "/static/livarava/img/projects/livarava-logo-black-400x400.png",
+            "type_title": "project"
+        }, {
+            "id": 29521,
+            "weight": 57,
+            "header": "LivaRava Webinar: Finance and Stocks - Lesson 2",
+            "title": "LivaRava Webinar: Finance and Stocks - Part 2",
+            "type": "event",
+            "data": 29521,
+            "icon": "/static/livarava/img/neurons/event.png",
+            "image": "/static/livarava/img/neurons/event.png",
+            "type_title": "event"
+        }, {
+            "id": 8828,
+            "weight": 47,
+            "header": "LivaRava Tutorial",
+            "title": "LivaRava Tutorial",
+            "type": "text",
+            "data": 8828,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 26051,
+            "weight": 46,
+            "header": "LivaRava Webinar: Finance and Stocks - Part 1",
+            "title": "LivaRava Finance Introduction",
+            "type": "event",
+            "data": 26051,
+            "icon": "/static/livarava/img/neurons/event.png",
+            "image": "/static/livarava/img/neurons/event.png",
+            "type_title": "event"
+        }, {
+            "id": 27083,
+            "weight": 43,
+            "header": "LivaRava Test",
+            "title": "LivaRava Test",
+            "type": "text",
+            "data": 27083,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 12181,
+            "weight": 23,
+            "header": "Las Vegas",
+            "title": "Las Vegas",
+            "type": "text",
+            "data": 12181,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 30121,
+            "weight": 9,
+            "header": "Las Vegas",
+            "title": "http://i.imgur.com/nVnzXva.jpg",
+            "type": "image",
+            "data": 30121,
+            "icon": "/static/livarava/img/neurons/image.png",
+            "image": "http://i.imgur.com/nVnzXva.jpg",
+            "type_title": "image"
+        }, {
+            "id": 23710,
+            "weight": 3,
+            "header": "Downtown Las Vegas",
+            "title": "Downtown Las Vegas",
+            "type": "text",
+            "data": 23710,
+            "icon": "/static/livarava/img/neurons/text.png",
+            "image": "/static/livarava/img/neurons/text.png",
+            "type_title": "text"
+        }, {
+            "id": 13239,
+            "weight": 3,
+            "header": "http://img0.svcdn.lasvegas.com/v2/cache/lasvegas/25C7DBB7FDEE98EB339313F2B55B68D5.jpg",
+            "title": "http://img0.svcdn.lasvegas.com/v2/cache/lasvegas/25C7DBB7FDEE98EB339313F2B55B68D5.jpg",
+            "type": "image",
+            "data": 13239,
+            "icon": "/static/livarava/img/neurons/image.png",
+            "image": "http://img0.svcdn.lasvegas.com/v2/cache/lasvegas/25C7DBB7FDEE98EB339313F2B55B68D5.jpg",
+            "type_title": "image"
+        }, {
+            "id": 13235,
+            "weight": 3,
+            "header": "http://flippins.com/wp-content/uploads/2014/02/las-vegas-night.jpg",
+            "title": "http://flippins.com/wp-content/uploads/2014/02/las-vegas-night.jpg",
+            "type": "image",
+            "data": 13235,
+            "icon": "/static/livarava/img/neurons/image.png",
+            "image": "http://flippins.com/wp-content/uploads/2014/02/las-vegas-night.jpg",
+            "type_title": "image"
+        }]
+    };
+
+    // sort neuron by creation date - same as id - descending
+    neuron.neurons = neuron.neurons.sort(function (neuron1, neuron2) {
+        return neuron2.id - neuron1.id;
+    });
+
+    neuron.neurons = neuron.neurons.map(function(neuron1){
+        if(neuron1.image.indexOf('/') === 0){
+            neuron1.image = 'https://www.livarava.com' + neuron1.image;
+        }
+        return neuron1;
+    });
+
+    window['text-neuron-data'] = neuron;
+
+})();
