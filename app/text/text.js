0c203761328f3ba59cf8f09521ce4b8e32bdb6b3
progand <lach@ukr.net>
2015-12-24 16:40:25 +0200

    [*]  filters from data

--- app/text/text.js
+++ app/text/text.js
@@ -18,5 +18,13 @@ angular.module('myApp.text', ['ui.router'])
     .controller('TextCtrl', ['$scope', 'neuron', function ($scope, neuron) {
         $scope.neuron = neuron;
         $scope.filters = _.chain(neuron.neurons).pluck('type').uniq().value();
-        $scope.filter = '';
+        $scope.filter = null;
+        $scope.filteredNeurons = neuron.neurons;
+
+        $scope.setFilter = function (newFilter) {
+            $scope.filter = newFilter;
+            $scope.filteredNeurons = $scope.neuron.neurons.filter(function (n) {
+                return !$scope.filter || n.type === $scope.filter;
+            });
+        }
     }]);
