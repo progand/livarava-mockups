07fed8619c02dfb25f553abc7e09fa0430448d35
progand <lach@ukr.net>
2015-12-24 09:15:35 +0200

    [*]  ngRoute replaced with angula-ui-router

--- app/index/index.js
+++ app/index/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('myApp.index', ['ngRoute'])
+angular.module('myApp.index', ['ui.router'])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/', {
+    .config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('/', {
+            url: '/',
             templateUrl: 'index/index.html',
             controller: 'IndexCtrl'
         });
