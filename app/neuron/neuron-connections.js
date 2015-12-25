1a2d3faa2ab36d487d626a02627659bd458d0d2e
progand <lach@ukr.net>
2015-12-25 13:08:43 +0200

    [*] link recognition

--- app/neuron/neuron-connections.js
+++ app/neuron/neuron-connections.js
@@ -23,15 +23,18 @@ angular.module('myApp.text')
                 type = 'text',
                 image = 'https://www.livarava.com/static/livarava/img/neurons/text.png';
 
-            if (text.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+            if (text.match(/\.(jpeg|jpg|gif|png)$/) !== null) {
                 type = 'image';
                 image = text;
-            } else if (text.match(/^http(s):\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/) != null) {
+            } else if (text.match(/^http(s):\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/) !== null) {
                 var videoId = text.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
                 if (videoId != null && videoId[1]) {
                     type = 'video';
                     image = 'http://img.youtube.com/vi/' + videoId[1] + '/default.jpg';
                 }
+            } else if(text.match( /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/) !== null){
+                type = 'link';
+                image = 'https://www.livarava.com/static/livarava/img/neurons/link.png';
             }
 
             $scope.neuron.neurons.unshift({
