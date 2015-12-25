2b5bf00a5beff08391f87739667425cd2e2e2efc
progand <lach@ukr.net>
2015-12-25 11:53:43 +0200

    [*] image and video neuron append

--- app/neuron/neuron-connections.js
+++ app/neuron/neuron-connections.js
@@ -19,11 +19,27 @@ angular.module('myApp.text')
         };
 
         $scope.addNewNeuron = function () {
+            var text = this.newNeuronText,
+                type = 'text',
+                image = 'https://www.livarava.com/static/livarava/img/neurons/text.png';
+
+            if (text.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+                type = 'image';
+                image = text;
+            } else if (text.match(/^http(s):\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/) != null) {
+                var videoId = text.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+                if (videoId != null && videoId[1]) {
+                    type = 'video';
+                    image = 'http://img.youtube.com/vi/' + videoId[1] + '/default.jpg';
+                }
+            }
+
             $scope.neuron.neurons.unshift({
                 id: Date.now(),
-                image: 'https://www.livarava.com/static/livarava/img/neurons/text.png',
-                type_title: 'text',
-                header: this.newNeuronText
+                header: text,
+                image: image,
+                type: type,
+                type_title: type
             });
             this.newNeuronText = '';
         }
