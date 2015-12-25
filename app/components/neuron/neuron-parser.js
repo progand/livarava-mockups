4ac187563fae6fd7283809027d32866d391fe46d
progand <lach@ukr.net>
2015-12-25 13:49:42 +0200

    [*] email recognition

--- app/components/neuron/neuron-parser.js
+++ app/components/neuron/neuron-parser.js
@@ -6,21 +6,22 @@ angular.module('myApp.text')
             var type = 'text',
                 image = 'https://www.livarava.com/static/livarava/img/neurons/text.png';
 
-            if (text.match(/\.(jpeg|jpg|gif|png)$/) !== null) {
+            var videoId = text.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+            if (videoId != null && videoId[1]) {
+                type = 'video';
+                image = 'http://img.youtube.com/vi/' + videoId[1] + '/default.jpg';
+            } else if (/\.(jpeg|jpg|gif|png)$/.test(text)) {
                 type = 'image';
                 image = text;
-            } else if (text.match(/^http(s):\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/) !== null) {
-                var videoId = text.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-                if (videoId != null && videoId[1]) {
-                    type = 'video';
-                    image = 'http://img.youtube.com/vi/' + videoId[1] + '/default.jpg';
-                }
-            } else if (text.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/) !== null) {
+            } else if (/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/.test(text)) {
                 type = 'link';
                 image = 'https://www.livarava.com/static/livarava/img/neurons/link.png';
-            } else if(text.match(/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i)){
+            } else if (/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i.test(text)) {
                 type = 'phone';
                 image = 'https://www.livarava.com/static/livarava/img/neurons/phone.png';
+            } else if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(text)) {
+                type = 'email';
+                image = 'https://www.livarava.com/static/livarava/img/neurons/email.png';
             }
 
             return {
