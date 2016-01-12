62bc2489c49138f2b2afafa07a2b19488f80a013
progand <lach@ukr.net>
2016-01-12 16:06:23 +0200

    [*] refractoring
    neuron connections arrow functions

--- app/neuron/controllers/neuron-connections.js
+++ app/neuron/controllers/neuron-connections.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp.text')
-    .controller('NeuronConnectionsCtrl', ['$scope', 'FileUploader', 'neuronParser', function ($scope, FileUploader, neuronParser) {
+    .controller('NeuronConnectionsCtrl', ['$scope', 'FileUploader', 'neuronParser', ($scope, FileUploader, neuronParser) => {
         $scope.filter = null;
         $scope.filteredNeurons = $scope.neuron.neurons;
         $scope.newNeuronText = '';
@@ -10,80 +10,68 @@ angular.module('myApp.text')
 
         /* File uploader settings */
         if (window.FileReader) {
-            var uploader = $scope.uploader = new FileUploader(),
+            let uploader = $scope.uploader = new FileUploader(),
                 audioUploader = $scope.audioUploader = new FileUploader();
 
 
-            uploader.onAfterAddingFile = function (fileItem) {
+            uploader.onAfterAddingFile = (fileItem) => {
                 var reader = new window.FileReader();
 
-                reader.onloadend = function (event) {
-                    $scope.$apply(function () {
+                reader.onloadend = event =>
+                    $scope.$apply(() =>
                         $scope.newImageNeuron = neuronParser.parse({
                             raw: event.target.result,
                             header: $scope.neuron.header
-                        });
-                    });
-                };
+                        })
+                    );
 
                 reader.readAsDataURL(fileItem._file);
             };
 
-            audioUploader.onAfterAddingFile = function (fileItem) {
+            audioUploader.onAfterAddingFile = (fileItem) => {
                 var reader = new window.FileReader();
 
-                reader.onloadend = function (event) {
-                    $scope.$apply(function () {
+                reader.onloadend = event =>
+                    $scope.$apply(() =>
                         $scope.newAudioNeuron = neuronParser.parse({
                             raw: event.target.result
-                        });
-                    });
-                };
+                        })
+                    );
+
 
                 reader.readAsDataURL(fileItem._file);
             };
 
-            uploader.onAfterAddingAll = function (addedFileItems) {
-                $scope.$apply(function () {
-                    $scope.newImageNeuron = null;
-                });
-            };
+            uploader.onAfterAddingAll = (addedFileItems) =>
+                $scope.$apply(() => $scope.newImageNeuron = null);
 
-            audioUploader.onAfterAddingAll = function (addedFileItems) {
-                $scope.$apply(function () {
-                    $scope.newAudioNeuron = null;
-                });
-            };
+            audioUploader.onAfterAddingAll = (addedFileItems) =>
+                $scope.$apply(() => $scope.newAudioNeuron = null);
         }
 
 
-        $scope.filters = function () {
-            return _.chain($scope.neuron.neurons).pluck('type').uniq().value();
-        };
+        $scope.filters = () => _.chain($scope.neuron.neurons).pluck('type').uniq().value();
 
-        $scope.filteredNeurons = function () {
-            return $scope.neuron.neurons.filter(function (n) {
-                return !$scope.filter || n.type === $scope.filter;
-            });
-        };
+        $scope.filteredNeurons = () =>
+            $scope.neuron.neurons.filter(n => !$scope.filter || n.type === $scope.filter
+            );
 
-        $scope.setFilter = function (newFilter) {
-            $scope.filter = newFilter;
-        };
-        $scope.showNewNeuronForm = function (newNeuronForm) {
-            $scope.newNeuronForm = newNeuronForm;
-        };
 
-        $scope.addNewNeuron = function (neuron) {
+        $scope.setFilter = newFilter => $scope.filter = newFilter;
+
+        $scope.showNewNeuronForm = newNeuronForm => $scope.newNeuronForm = newNeuronForm;
+
+        $scope.addNewNeuron = neuron => {
             $scope.neuron.neurons.unshift(neuron);
             $scope.clearNeuron(neuron);
         };
         $scope.onNewNeuronTextChange = function () {
             $scope.newNeuron = neuronParser.parse({raw: this.newNeuronText});
         };
-        $scope.deleteFromNeurons = function (neuron) {
+
+        $scope.deleteFromNeurons = neuron =>
             $scope.neuron.neurons = _.without($scope.neuron.neurons, neuron)
-        };
+
         $scope.clearNeuron = function (neuron) {
             if ($scope.newNeuron === neuron) {
                 this.newNeuronText = '';
