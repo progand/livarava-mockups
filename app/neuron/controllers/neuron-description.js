8a780bdc420515c9b5981786bf4474b6d8370f4d
00FullZero00 <00FullZero00@gmail.com>
2016-01-15 15:35:00 +0200

    [*] Editable description

--- app/neuron/controllers/neuron-description.js
+++ app/neuron/controllers/neuron-description.js
@@ -3,5 +3,48 @@
 angular.module('myApp.text')
     .controller('NeuronDescriptionCtrl', ['$scope', function ($scope) {
       //Description controller
-        $scope.message = 'Put description template here';
+      //  $scope.message = 'Put description template here';
+        $scope.linkDescription = false;
+        $scope.textDescription = false;
+        $scope.newDescriptionText = '';
+
+        if ($scope.newDescriptionText == '') {
+            $scope.linkDescription = false;
+        } else {
+            $scope.linkDescription = true;
+        }
+
+        $scope.addDescription = data => {
+            $scope.newDescriptionText = data;
+            $scope.newDescription = false;
+            if (data) {
+                $scope.linkDescription = true;
+                $scope.textDescription = true;
+            } else {
+                $scope.linkDescription = false;
+                $scope.textDescription = false;
+            }
+        };
+
+        $scope.onDescriptionEditLink = () => {
+            console.log($scope.textDescription);
+            $scope.newDescription = !$scope.newDescription;
+            $scope.linkDescription = !$scope.linkDescription;
+            $scope.textDescription = !$scope.textDescription;
+            if($scope.textDescription == true) {
+                $scope.textDescription = false;
+            }
+        };
+
+        $scope.onCloseForm = data => {
+            $scope.newDescription = false;
+            if (data) {
+                $scope.linkDescription = true;
+                $scope.textDescription = true;
+            } else {
+                $scope.linkDescription = false;
+                $scope.textDescription = false;
+            }
+        };
+
     }]);
