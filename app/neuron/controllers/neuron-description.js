bbe9dcd34d1942bb6788d819bca580a8f46f891c
progand <lach@ukr.net>
2016-01-20 12:11:22 +0200

    [*] neuron description refractored

--- app/neuron/controllers/neuron-description.js
+++ app/neuron/controllers/neuron-description.js
@@ -3,49 +3,10 @@
 angular.module('myApp.text')
     .controller('NeuronDescriptionCtrl', ['$scope', function ($scope) {
         //Description controller
-        $scope.textDescription = false;
-        $scope.newDescriptionText = '';
+        $scope.description = '';
+        $scope.editing = false;
 
-        $scope.linkDescription = () => {
-            return !!$scope.newDescriptionText;
+        $scope.setEditing = function (editing) {
+            $scope.editing = editing;
         };
-
-        $scope.addDescription = data => {
-            $scope.newDescriptionText = data;
-            $scope.newDescription = false;
-            if (data) {
-                $scope.onCheckEl(true);
-                $scope.textDescription = true;
-            } else {
-                $scope.onCheckEl(false);
-                $scope.textDescription = false;
-            }
-        };
-
-        $scope.onDescriptionEditLink = () => {
-            $scope.newDescription = !$scope.newDescription;
-            $scope.onCheckEl(true);
-            $scope.textDescription = !$scope.textDescription;
-            if($scope.textDescription == true) {
-                $scope.textDescription = false;
-            }
-        };
-
-        $scope.onCloseForm = data => {
-            $scope.newDescription = false;
-            if (data) {
-                $scope.onCheckEl(true);
-                $scope.textDescription = true;
-            } else {
-                $scope.onCheckEl(false);
-                $scope.textDescription = false;
-            }
-        };
-
-        $scope.onCheckEl = b => {
-            $scope.linkDescription = () => {
-                return b;
-            };
-        };
-
     }]);
